77ed084d32b4600747ba7a5b04f2d1049c8b4d2f
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-05-10 15:13:45 +0530

    created a banner

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,1 +1,17 @@
 #! /usr/bin/env node
+import figlet from "figlet";
+import { Command } from "commander";
+import chalk from "chalk";
+
+function banner() {
+  console.log(
+    chalk.bold.underline.cyanBright(
+      figlet.textSync("md-blog", {
+        font: "Sub-Zero",
+      })
+    )
+  );
+  console.log(chalk.italic("Write, Manage and Publish. All at one place."));
+}
+
+banner();
