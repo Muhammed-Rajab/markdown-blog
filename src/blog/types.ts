c812dffe376203f9e9d7ff6a4b81609a5d4d6fb4
Muhammed Rajab <muhammedrajab101@gmail.com>
2024-05-10 21:26:09 +0530

    let's add cover to our blog

--- src/blog/types.ts
+++ src/blog/types.ts
@@ -2,6 +2,7 @@ export type Metadata = {
   id: string;
   title: string;
   desc: string;
+  cover?: string;
   slug: string;
   createdAt: Date;
   editedAt: Date;
@@ -15,9 +16,11 @@ export type CreateBlogOptions = {
   title: string;
   desc: string;
   draft: boolean;
+  cover?: string;
 };
 export type UpdateBlogOptions = {
   title?: string;
   desc?: string;
   draft?: boolean;
+  cover?: string;
 };
